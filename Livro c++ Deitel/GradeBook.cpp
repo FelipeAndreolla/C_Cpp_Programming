1c37fe233246cef606544996af7ce0b5e1936b68
Felipe Andreolla <132094407+FelipeAndreolla@users.noreply.github.com>
2023-09-01 16:07:12 -0300

    versão atualizada 1.1

--- Livro c++ Deitel/GradeBook.cpp
+++ Livro c++ Deitel/GradeBook.cpp
@@ -2,6 +2,7 @@
 // Cria um objeto GradeBook e chama sua função displayMessage.
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,17 +10,22 @@ using namespace std;
 class GradeBook
 {
     public:
-        void MostraMensagem(){
+        void MostraMensagem( string nomeCurso ){
             // função que exibe uma mensagem de boas-vindas ao usuário do GradeBook
-            cout << "Bem vindo ao livro de notas." << endl;
+            cout << "Bem vindo ao livro de notas de " << nomeCurso << "!" << endl;
         }
 };
 
 
 int main(){
 
+    string nomeDoCurso;
     GradeBook myGradeBook; // cria um objeto GradeBook chamado myGradeBook
-    myGradeBook.MostraMensagem(); // chama a função displayMessage do objeto
+    
+    cout << "Por favor, informe o nome do curso: \n\n" << endl;
+    cin >> nomeDoCurso;
+
+    myGradeBook.MostraMensagem( nomeDoCurso ); // chama a função displayMessage do objeto
 
     return 0;
 }
